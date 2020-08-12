3caf08cfb03a31aa964cbb24f4865f8d6be29e73
Brian De Maio <bdemaio1@gmail.com>
2020-08-11 23:32:50 -0400

    Setting up reducer for job fetch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
-//import "./App.css";
+import useFetchJobs from "./useFetchJobs";
+import { Container } from "react-bootstrap";
 
 function App() {
-  return <div className="App"></div>;
+  const { jobs, loading, error } = useFetchJobs({}, 1);
+  return (
+    <Container>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error, Try Refresh</h1>}
+      <h1>{jobs.length}</h1>
+    </Container>
+  );
 }
 
 export default App;
